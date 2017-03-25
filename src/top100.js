34f62b018598eda7fced5baeeba72cc690bdf31a
serenader <xyslive@gmail.com>
2017-03-25 16:02:27 +0800

    top100 api support category

--- src/top100.js
+++ src/top100.js
@@ -1,13 +1,39 @@
 const cheerio = require('cheerio')
 
 const TOP_100_URL = require('./constants').TOP_100_URL
+const categories = require('./constants').categories
 const utils = require('./utils')
 const parseTable = utils.parseTable
 const request = utils.request
 
-module.exports = function listTop100() {
+module.exports = function listTop100(category) {
+  if (typeof category === 'string') {
+    category = [category]
+  } else {
+    category = category || []
+  }
+  let categoryIds = []
+
+  category.forEach(c => {
+    if (typeof c === 'string') {
+      categoryIds = categoryIds.concat(categories[c])
+    } else {
+      categoryIds.push(c)
+    }
+  })
+
+  const tmpMap = {}
+  const queryString = categoryIds.filter(item => {
+    if (!tmpMap[item]) {
+      tmpMap[item] = true
+      return true
+    } else {
+      return false
+    }
+  }).map(item => `category[]=${item}`).join('&')
+
   return request({
-    url: `${TOP_100_URL}`,
+    url: queryString ? `${TOP_100_URL}?${queryString}` : TOP_100_URL,
   }).then(body => {
     const $ = cheerio.load(body)
     const table = $('table.lista2t')
--- test/top100.spec.js
+++ test/top100.spec.js
@@ -10,4 +10,23 @@ describe('list top 100 api test', function() {
       data.length.should.equal(100)
     })
   })
+
+  it('should list 100 tv records', function() {
+    return top100('tv').then(data => {
+      data.length.should.equal(100)
+      data[0]['Cat.'].should.equal('tv')
+    })
+  })
+
+  it('should list 100 tv and movies', function() {
+    return top100(['tv', 'movies']).then(data => {
+      data.length.should.equal(100)
+    })
+  })
+
+  it('should list 100 xxx and tv', function() {
+    return top100([4, 18]).then(data => {
+      data.length.should.equal(100)
+    })
+  })
 })
